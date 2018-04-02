822a062ca165accb8759d31debf73da27a1a5fb6
Noah Valley <noah@boeggsli.ch>
2018-04-02 21:35:10 +0200

    shortfile

--- jups/servers/api/controllers/files.js
+++ jups/servers/api/controllers/files.js
@@ -23,34 +23,10 @@ var files = {
 	},
 	upload : function(req, res, next){
 		console.log('files.upload');
-	
-		// create an incoming form object
 		var form = new formidable.IncomingForm();
-		
-		// specify that we want to allow the user to upload multiple files in a single request
-		form.multiples = true;
-		
-		// store all uploads in the /uploads directory
-		form.uploadDir = path.join(__dirname, '/uploads');
-		
-		// every time a file has been uploaded successfully,
-		// rename it to it's orignal name
-		form.on('file', function(field, file) {
-		fs.rename(file.path, path.join(form.uploadDir, file.name));
-		});
-		
-		// log any errors that occur
-		form.on('error', function(err) {
-			console.log('An error has occured: \n' + err);
-		});
-		
-		// once all the files have been uploaded, send a response to the client
-		form.on('end', function() {
-			res.end('success');
-		});
-		
-		// parse the incoming request containing the form data
+		form.uploadDir = path.join(__dirname, '../../../ressources/uploads/');
 		form.parse(req);
+		next();
 	}
 }
 
