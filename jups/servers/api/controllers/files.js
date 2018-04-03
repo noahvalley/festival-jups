e7480615cb1815f1adcb637d56363bc54905bada
Noah Valley <noah@boeggsli.ch>
2018-04-03 23:01:53 +0200

    fileupload funktionierend

--- jups/servers/api/controllers/files.js
+++ jups/servers/api/controllers/files.js
@@ -25,8 +25,24 @@ var files = {
 		console.log('files.upload');
 		var form = new formidable.IncomingForm();
 		form.uploadDir = path.join(__dirname, '../../../ressources/uploads/');
+		form.on('file', function(field, file) {
+			fs.rename(file.path, path.join(form.uploadDir, file.name), err => {
+				if (err){
+					console.log(err);
+				}
+			});
+		});
+		
+		// log any errors that occur
+		form.on('error', function(err) {
+			next({error: true, number: 9001, message: 'something with the upload', suberror: err});
+		});
+		
+		// once all the files have been uploaded, send a response to the client
+		form.on('end', function() {
+			next();
+		});
 		form.parse(req);
-		next();
 	}
 }
 
