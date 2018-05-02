295475491cf736c15fcb3c77e4021b2599c0edbf
jimynu <kim@bylr.ch>
2018-05-02 21:20:50 +0200

    design adjustments

--- ressources/frontend/src/routes/Orte/index.js
+++ ressources/frontend/src/routes/Orte/index.js
@@ -18,35 +18,38 @@ const Orte = () => {
             <div className="spielorte">
 
               <ul>
-                <li><div className="buchstabe">A</div>
-                <strong>Kammgarn &amp; Vebikus</strong> <em>(Festivalzentrum)</em>
-                <br/>Baumgartenstrasse 19
-                <br/><a href="http://www.kammgarn.ch/">www.kammgarn.ch</a></li>
+                <li>
+                  <div className="buchstabe">A</div>
+                  <strong>Kammgarn &amp; Vebikus</strong> <em>(Festivalzentrum)</em>
+                  <br/>Baumgartenstrasse 19
+                  <br/><a href="http://www.kammgarn.ch/">www.kammgarn.ch</a>
+                </li>
 
-                <li><div className="buchstabe">B</div>
-                <strong>MKS Musikschule Schaffhausen</strong>
-                <br/>Rosengasse 26
-                <br/><a href="http://www.mksh.ch/">www.mksh.ch</a></li>
+                <li>
+                  <div className="buchstabe">B</div>
+                  <strong>Musikschule MKS</strong>
+                  <br/>Rosengasse 26
+                  <br/><a href="http://www.mksh.ch/">www.mksh.ch</a>
+                </li>
 
-                <li><div className="buchstabe">C</div>
-                <strong>Museum zu Allerheiligen</strong>
-                <br/>Klosterstrasse 16
-                <br/><a href="http://www.allerheiligen.ch">www.allerheiligen.ch</a></li>
+                <li>
+                  <div className="buchstabe">C</div>
+                  <strong>Museum zu Allerheiligen</strong>
+                  <br/>Klosterstrasse 16
+                  <br/><a href="http://www.allerheiligen.ch">www.allerheiligen.ch</a>
+                </li>
 
-                <li><div className="buchstabe">D</div>
-                <strong>Stadttheater Schaffhausen</strong>
-                <br/>Herrenacker 23
-                <br/><a href="http://www.stadttheater-sh.ch">www.stadttheater-sh.ch</a></li>
+                <li>
+                  <div className="buchstabe">D</div>
+                  <strong>Probebühne Cardinal</strong>
+                  <br/>Bachstrasse 75
+                  <div style={{ clear: 'both' }} />
+                </li>
 
-                <li><div className="buchstabe">E</div>
-                <strong>Haberhaus Bühne</strong>
-                <br/>Neustadt 51
-                <br/><a href="http://www.haberhaus.ch/buehne">www.haberhaus.ch/buehne</a></li>
-
-                <li><div className="buchstabe">F</div>
-                <strong>Radio Munot</strong>
-                <br/>Stadthausgasse 11
-                <br/><a href="http://www.radiomunot.ch">www.radiomunot.ch</a></li>
+                <li>
+                  <div className="buchstabe">E</div>
+                  <strong>Herrenacker</strong>
+                </li>
               </ul>
 
             </div>
